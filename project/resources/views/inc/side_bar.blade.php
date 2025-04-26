65c9e38647709f98ed074c87572b00c6b61039f4
Vladyslav-Mihulov <vlad.migula@gmail.com>
2025-04-26 15:50:30 +0200

    commit1

---
+++ project/resources/views/inc/side_bar.blade.php
@@ -0,0 +1,37 @@
+<div id="sidebar">
+                    <div class="inner">
+
+			<!-- Menu -->
+			<nav id="menu">
+			<header class="major">
+				<h2>Menu</h2>
+			</header>
+				<ul>
+                                    <li><a href="{{ route('home') }}">Homepage</a></li>
+                                    <!-- <li><a href="{$conf->action_root}homePage">Login</a></li> -->						
+				</ul>
+                            
+                        <!-- Section -->
+                        <section style="margin-top: 30px;">
+                            <header class="major">
+                                <h2>Skontaktuj się z nami</h2>
+                            </header>
+                                    
+                            <ul class="contact">
+				<li class="icon solid fa-envelope"><a href="#">..@..</a></li>
+				<li class="icon solid fa-phone">+00-000-000-000</li>
+				<li class="icon solid fa-home">....<br />
+				...</li>
+                            </ul>
+			</section>
+			</nav>
+
+							
+			<!-- Footer -->
+                            <footer id="footer">
+				<p class="copyright">&copy; Author: Vladyslav Mihulov </p>
+                            </footer>
+
+                    </div>
+		</div>
+
